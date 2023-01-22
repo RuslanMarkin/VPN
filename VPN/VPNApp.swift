bb942c357f79030461bd063daa28099d75337097
RuslanMarkin <ruslan.markin.work@gmail.com>
2023-01-23 01:18:44 +0300

    Initial Commit

---
+++ VPN/VPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPNApp.swift
+//  VPN
+//
+//  Created by Ruslan on 23.01.23.
+//
+
+import SwiftUI
+
+@main
+struct VPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
